66259b1c94c50ddb3826cc730256e067f595bbb8
Placide Muhemedi salumu <94157141+muhemedi-placide@users.noreply.github.com>
2026-06-13 22:28:25 +0200

    Add footer utility page routes

--- web/routes/web.php
+++ web/routes/web.php
@@ -13,6 +13,22 @@ Route::get('/{locale}/blog', [ShopController::class, 'blog'])
     ->whereIn('locale', ['fr', 'en'])
     ->name('blog.index');
 
+Route::get('/{locale}/livraison', [ShopController::class, 'delivery'])
+    ->whereIn('locale', ['fr', 'en'])
+    ->name('pages.delivery');
+
+Route::get('/{locale}/mentions-legales', [ShopController::class, 'legalNotice'])
+    ->whereIn('locale', ['fr', 'en'])
+    ->name('pages.legal');
+
+Route::get('/{locale}/conditions-utilisation', [ShopController::class, 'terms'])
+    ->whereIn('locale', ['fr', 'en'])
+    ->name('pages.terms');
+
+Route::get('/{locale}/paiement-securise', [ShopController::class, 'securePayment'])
+    ->whereIn('locale', ['fr', 'en'])
+    ->name('pages.payment');
+
 Route::get('/{locale}/products/{slug}', [ShopController::class, 'show'])
     ->whereIn('locale', ['fr', 'en'])
     ->name('products.show');
