162e35c34824fdc5508fc6b6f5c7a5a388bab0fd
Placide Muhemedi salumu <94157141+muhemedi-placide@users.noreply.github.com>
2026-06-13 22:10:26 +0200

    Add standalone about and blog routes

--- web/routes/web.php
+++ web/routes/web.php
@@ -5,6 +5,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ShopController::class, 'home'])->name('home');
 
+Route::get('/{locale}/about', [ShopController::class, 'about'])
+    ->whereIn('locale', ['fr', 'en'])
+    ->name('pages.about');
+
+Route::get('/{locale}/blog', [ShopController::class, 'blog'])
+    ->whereIn('locale', ['fr', 'en'])
+    ->name('blog.index');
+
 Route::get('/{locale}/products/{slug}', [ShopController::class, 'show'])
     ->whereIn('locale', ['fr', 'en'])
     ->name('products.show');
